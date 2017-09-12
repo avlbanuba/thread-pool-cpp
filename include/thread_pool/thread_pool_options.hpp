3c227ae9643758ff81f012b9fadcce09b399c394
Anton Lechanka <anton.lechenko@banuba.com>
2017-09-12 21:38:46 +0300

    build fix for clang on MacOS

--- include/thread_pool/thread_pool_options.hpp
+++ include/thread_pool/thread_pool_options.hpp
@@ -3,74 +3,60 @@
 #include <algorithm>
 #include <thread>
 
-namespace tp
-{
+namespace tp {
 
 /**
  * @brief The ThreadPoolOptions class provides creation options for
  * ThreadPool.
  */
-class ThreadPoolOptions
-{
-public:
-    /**
-     * @brief ThreadPoolOptions Construct default options for thread pool.
-     */
-    ThreadPoolOptions();
+    class ThreadPoolOptions {
+    public:
+        /**
+         * @brief ThreadPoolOptions Construct default options for thread pool.
+         */
+        ThreadPoolOptions()
+                : m_thread_count(std::max<size_t>(1u, std::thread::hardware_concurrency())), m_queue_size(1024u) {
+        }
 
-    /**
-     * @brief setThreadCount Set thread count.
-     * @param count Number of threads to be created.
-     */
-    void setThreadCount(size_t count);
+        /**
+         * @brief ThreadPoolOptions Construct configurable options for thread pool.
+         */
+        ThreadPoolOptions(size_t thread_num, size_t q_size)
+                : m_thread_count(thread_num), m_queue_size(1024u) {
+        }
 
-    /**
-     * @brief setQueueSize Set single worker queue size.
-     * @param count Maximum length of queue of single worker.
-     */
-    void setQueueSize(size_t size);
+        /**
+         * @brief setThreadCount Set thread count.
+         * @param count Number of threads to be created.
+         */
+        void setThreadCount(size_t count) {
+            m_thread_count = std::max<size_t>(1u, count);
+        }
 
-    /**
-     * @brief threadCount Return thread count.
-     */
-    size_t threadCount() const;
+        /**
+         * @brief setQueueSize Set single worker queue size.
+         * @param count Maximum length of queue of single worker.
+         */
+        void setQueueSize(size_t size) {
+            m_queue_size = std::max<size_t>(1u, size);
+        }
 
-    /**
-     * @brief queueSize Return single worker queue size.
-     */
-    size_t queueSize() const;
+        /**
+         * @brief threadCount Return thread count.
+         */
+        size_t threadCount() const {
+            return m_thread_count;
+        }
 
-private:
-    size_t m_thread_count;
-    size_t m_queue_size;
-};
+        /**
+         * @brief queueSize Return single worker queue size.
+         */
+        size_t queueSize() const {
+            return m_queue_size;
+        }
 
-/// Implementation
-
-ThreadPoolOptions::ThreadPoolOptions()
-    : m_thread_count(std::max<size_t>(1u, std::thread::hardware_concurrency()))
-    , m_queue_size(1024u)
-{
-}
-
-void ThreadPoolOptions::setThreadCount(size_t count)
-{
-    m_thread_count = std::max<size_t>(1u, count);
-}
-
-void ThreadPoolOptions::setQueueSize(size_t size)
-{
-    m_queue_size = std::max<size_t>(1u, size);
-}
-
-size_t ThreadPoolOptions::threadCount() const
-{
-    return m_thread_count;
-}
-
-size_t ThreadPoolOptions::queueSize() const
-{
-    return m_queue_size;
-}
-
+    private:
+        size_t m_thread_count;
+        size_t m_queue_size;
+    };
 }
